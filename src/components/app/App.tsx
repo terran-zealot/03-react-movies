d1b612677627562268b8379849306e27e6a6cf35
Сергій Власов <vikinglaurasun@gmail.com>
2025-05-26 19:20:03 +0300

    fix naming number 3

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -3,7 +3,7 @@ import SearchBar from '../SearchBar/SearchBar'
 import { type Movie } from '../../types/movie';
 import MovieGrid from '../MovieGrid/MovieGrid';
 import { fetchMovies } from '../../services/movieService';
-import ErrorMessage from '../ErrorMessage/ErrorMessage';
+import ErrorMessage from '../ErrorMessage/ErrorMessage'
 import toast, { Toaster } from 'react-hot-toast';
 import Loader from '../Loader/Loader';
 import MovieModal from '../MovieModal/MovieModal';
